e47448cfa2e525be0717fa044c23dbeb18256ea4
Felipe Somensi Duarte <110866742+FelipeSomensi@users.noreply.github.com>
2022-10-08 19:07:24 -0300

    Update ContasMes.java

--- ContasMes.java
+++ ContasMes.java
@@ -1,26 +1,19 @@
-package ContasMes;
+package Contas;
 
 public class ContasMes {
 
+	
 	private double dinheiroSaldo;  //mostra o saldo no banco
+	public String mes;             // representa o mes 
 	private double gastoMes;			//gasto do mes
 	private double gasto;			// gasto generico
 	
-	
 
-	
-	
-	public double getDinheiroSaldo() {
-		return dinheiroSaldo;
-	}
-	public void setDinheiroSaldo(double dinheiroSaldo) {
-		this.dinheiroSaldo = dinheiroSaldo;
-	}
-	public double getGastoMes() {
-		return gastoMes;
-	}
-	public void setGastoMes(double gastoMes) {
-		this.gastoMes = gastoMes;
+
+	public void setMes(String mes) {  //define o mes// Esta inserido dentro do ano
+		this.mes = mes;
+		System.out.println("Voce criou um novo mes: " + mes);
+		
 	}
 	
 	public void lancarGasto(double gasto) {
