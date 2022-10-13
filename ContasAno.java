f2fcbf610f13ce89cef9d3a95ba8bb0bb9d0b483
Felipe Somensi Duarte <110866742+FelipeSomensi@users.noreply.github.com>
2022-10-12 21:57:01 -0300

    Update ContasAno.java

--- ContasAno.java
+++ ContasAno.java
@@ -2,11 +2,10 @@ package Contas;
 
 public class ContasAno{
 	
-
 	public int ano;	// representa o ano
 	public int mesDoAno; //scanner
 	public String nomeMesDoAno;
-	public ContasMes ContasMes;
+	public ContasMes ContasDoMes;
 	
 	//fazer array para os meses do ano.
 	
@@ -14,6 +13,7 @@ public class ContasAno{
 	public ContasAno(int ano, int mesDoAno) {  //define o ano// Dentro do objeto ano terá um objeto contasMes para cada mes do ano
 		this.ano = ano;
 		this.mesDoAno = mesDoAno;
+	//	ContasAno.ContasDoMes = 23; HAAAAAAAAAAAAAAA
 		if(mesDoAno == 1) {
 		nomeMesDoAno = "Janeiro";
 			} else if(mesDoAno == 2) {
@@ -24,9 +24,8 @@ public class ContasAno{
 						nomeMesDoAno = "Abril";
 						} else if(mesDoAno == 5) {
 							nomeMesDoAno = "Maio";
-		
+		}
 		System.out.println("Voce esta no ano: " + ano);
 		System.out.println("Voce esta no mes: " + nomeMesDoAno);
-		}
 	}	
 }
