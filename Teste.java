f7b2b3bc07bd99b5d1569fad4cbcdd208039fd52
Felipe Somensi Duarte <110866742+FelipeSomensi@users.noreply.github.com>
2022-10-23 17:29:00 -0300

    Update Teste.java
    adição de scanner para testar alguns aprendizados

--- Teste.java
+++ Teste.java
@@ -1,21 +1,34 @@
-package TestesContas;
+package Testes;
 
-import Contas.ContasAno;
-import Contas.ContasMes;
-import Contas.Usuario;
+import java.util.Scanner;
 
-public class Teste {
+import ContasMes.Usuario;
+
+public class TesteContasMes {
 
 	public static void main(String[] args) {
 		
-		Usuario Felipe = new Usuario("Felipe Somensi");
-		ContasAno contasFelipe = new ContasAno(2022,1);
-		//ContasMes contaMesFelipe = new ContasMes(contasFelipe);
-		contasFelipe.ContasDoMes.lancarGasto(20.00, "corrida de uber");
-		//contasFelipe.ContasDoMes.lancarGasto(15, "compra Remedio");
+		Scanner scan = new Scanner(System.in);
+		int nMes; int nAno; double nValor; String informacao;
+		nMes=scan.nextInt();
+		nAno=scan.nextInt();
+		nValor=scan.nextDouble();
+		informacao=scan.nextLine();
+		
+		//------------------------
+		System.out.println("CONTAS MENSAIS");
+		System.out.println("Por favor crie um usuário informando seu nome");
 		
 		
-	
+		
+		Usuario Felipe = new Usuario("Felipe");
+		Felipe.lancarContaMes(2022, 22, 20.2, "teste"); // anoGasto,mesGasto,gasto,obs
+		//Felipe.lancarContaMes(2022, 2, 100, "cabeleleilo");
+
+		
+		Felipe.informaGastos(2022, 2); //anoGasto, mesGasto
+		
+		
 
 	}
 
