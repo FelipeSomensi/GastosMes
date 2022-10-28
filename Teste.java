014895abe07780ec2362e1c9c6b7a9af61b334c8
Felipe Somensi Duarte <110866742+FelipeSomensi@users.noreply.github.com>
2022-10-28 00:07:13 -0300

    Update Teste.java
    Adição de Switch e estrutura de repetição para looping de execução do programa

--- Teste.java
+++ Teste.java
@@ -8,28 +8,60 @@ public class TesteContasMes {
 
 	public static void main(String[] args) {
 		
+		
 		Scanner scan = new Scanner(System.in);
-		int nMes; int nAno; double nValor; String informacao;
-		nMes=scan.nextInt();
-		nAno=scan.nextInt();
-		nValor=scan.nextDouble();
-		informacao=scan.nextLine();
+	    int nMes; int nAno = 0; double nValor = 0; String informacao; 
+		int escolhaPrograma = 99;
+		
+	
+		Usuario Felipe = new Usuario("Felipe");
+		
 		
 		//------------------------
-		System.out.println("CONTAS MENSAIS");
-		System.out.println("Por favor crie um usuário informando seu nome");
+		while(escolhaPrograma != 0 ) {
+			
+			System.out.println("CONTAS MENSAIS");
+			System.out.println("1 -- para adicionar gasto");
+			System.out.println("2 -- para verificar gastos");
+			System.out.println("0 -- Para Sair  ");
+			escolhaPrograma = scan.nextInt();
+			
+			switch(escolhaPrograma) {
+			case 1:
+				System.out.println("Por favor informe o ano:");
+				nAno=scan.nextInt();
+				
+				System.out.println("Por favor informe o mes:");
+				nMes=scan.nextInt();
+				try {
+				System.out.println("Por favor informe o valor");
+				nValor=scan.nextDouble();
+				} catch(java.util.InputMismatchException exception) {
+					System.out.println("Eh necessario usar virgula, nao ponto");
+					//System.exit(nAno); //forca o cancelamento do codigo
+					break;
+				}
+				System.out.println("Por favor informe a informacao sobre o gasto");
+				informacao = scan.next();
+				
+				Felipe.lancarContaMes(nAno, nMes, nValor, informacao); // anoGasto,mesGasto,gasto,obs
+				//Felipe.lancarContaMes(2022, 2, 100, "cabeleleilo");
+				break;
+			
+			case 2:
+				Felipe.informaGastos(2022, 2); //anoGasto, mesGasto
+				break;
+				
+			case 0:
+				System.out.println("Voce escolheu sair");
+				System.exit(nAno); //forca o cancelamento do codigo
+			
+			
+			
+			}
 		
+		}
 		
-		
-		Usuario Felipe = new Usuario("Felipe");
-		Felipe.lancarContaMes(2022, 22, 20.2, "teste"); // anoGasto,mesGasto,gasto,obs
-		//Felipe.lancarContaMes(2022, 2, 100, "cabeleleilo");
-
-		
-		Felipe.informaGastos(2022, 2); //anoGasto, mesGasto
-		
-		
-
 	}
 
 }
